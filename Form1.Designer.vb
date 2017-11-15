e407e3139976c3514f928d9a0abfefba76df7dc4
MXMY <mxmingya@gmail.com>
2017-11-14 21:19:22 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(0, 0)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(165, 33)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 110)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 111)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.MaskedTextBox1.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(140, 62)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents MaskedTextBox1 As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,92 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim pres() As String = IO.File.ReadAllLines("C:\Users\maxxx950\Desktop\USStates.txt")
+        '32
+        ListBox1.Items.Add(pres(0)) 'first president
+        For i As Integer = 0 To 5 ' first 6 presidents
+            ListBox1.Items.Add(pres(i))
+        Next
+        ListBox1.Items.Add(pres(1)) 'second president
+        ListBox1.Items.Add(pres(9)) '10th president
+        ListBox1.Items.Add(pres(pres.Length - 1))
+        ListBox1.Items.Add(pres(Array.IndexOf(pres, "James Monroe")))
+        For i As Integer = pres.Length - 1 To pres.Length - 6 'last 5 presidents
+            ListBox1.Items.Add(pres(i))
+        Next
+
+        '38
+        Dim numbers() As Integer
+        Dim counter As Integer = 0
+        For Each num As Integer In numbers
+            If num Mod 2 = 0 Then
+                counter = counter + 1
+            End If
+        Next
+
+        '7.4.22
+        Dim allData(,) As Integer = {{23034, 23850}, {13980, 14027}, {11131, 11131}, {7566, 7542}, {7250, 7253}}
+        Dim count As Integer = 0
+        For i As Integer = 0 To 4
+            For j As Integer = 0 To 1
+                count = count + allData(i, j)
+            Next
+        Next
+        TextBox1.Text = count
+    End Sub
+
+    '46
+    Private Function checkOrder()
+        Dim states() As String = IO.File.ReadAllLines("C:\Users\maxxx950\Desktop\USStates.txt")
+        Dim cloned() As String = states.Clone()
+        Array.Sort(cloned)
+        For i As Integer = 0 To states.Length - 1
+            If (states(i) = cloned(i)) Then
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+
+        Catch ex As Exception
+            MsgBox("There is an error while opening the File")
+        End Try
+
+        'For Each president As String In pres
+        'MsgBox.show(president)
+        'Next
+    End Sub
+
+    '7.3 20
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        DataGridView1.Columns("abbre").HeaderText = "State Abbreviation"
+        DataGridView1.Columns("name").HeaderText = "State"
+        Dim states() As String = IO.File.ReadAllLines("C:\Users\maxxx950\Desktop\USStates.txt")
+        For Each state As String In states
+            Dim line() As String = state.Split(",")
+            Dim name As String = line(0) 'Delaware
+            Dim abbre As String = line(1) 'DE
+            For i As Integer = 0 To 1
+                Dim c11 As Char = name(0) 'D
+                Dim c12 As Char = Char.ToUpper(name(1)) 'E
+                Dim c21 As Char = abbre(0) 'D
+                Dim c22 As Char = abbre(1) 'E
+                If c11 = c21 And c12 = c22 Then
+                    DataGridView1.Rows.Add(abbre, name)
+                End If
+            Next
+        Next
+    End Sub
+
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
